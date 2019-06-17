aa8a14cf3c09ef7000e60328eb3b4b56528aab2b
splcoder <www3.splcoder@gmail.com>
2019-06-17 17:27:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/termux/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.termux;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/*
+ * Utilizar PostgreSQL en Android con JDBC
+ * www.programacion.com.py - Recursos y documentación para desarrolladores - By Rodrigo Paszniuk
+ * PD: Para agregar una libreria .jar cualquiera al proyecto solamente se debe agregar a la carpeta libs del mismo.
+ * Para actualizar cambios hacer click derecho al proyecto y luego  click izquierdo a Refresh.
+ *
+ * Gradle -> dependencies: poner el nombre del jdbc (jar)
+ * 		implementation files('libs/postgresql-42.2.5')
+ *
+ *
+ *
+ * 	VER:	https://wiki.termux.com/wiki/Main_Page
+ * 			https://pgolub.wordpress.com/2017/11/24/postgresql-on-android/
+ * 			https://wiki.termux.com/wiki/Postgresql
+ * 			https://gist.github.com/Kartones/dd3ff5ec5ea238d4c546
+ * 			https://www.a2hosting.es/kb/developer-corner/postgresql/connect-to-postgresql-from-the-command-line
+ * 			https://www.makeuseof.com/tag/use-linux-command-line-android-termux/
+				Starting the database
+					pg_ctl -D $PREFIX/var/lib/postgresql start
+				Similarly stop the database using
+					pg_ctl stop
+
+				-----------------------------------------------------------
+				User-PC:~$ psql postgres
+				psql (9.5.7)
+				Type "help" for help.
+
+				postgres=# help
+				You are using psql, the command-line interface to PostgreSQL.
+				Type:  \copyright for distribution terms
+					   \h for help with SQL commands
+					   \? for help with psql commands
+					   \g or terminate with semicolon to execute query
+					   \q to quit
+				postgres=# \? createdb
+ */
+public class MainActivity extends AppCompatActivity {
+
+	TextView txtData;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		txtData = findViewById( R.id.txtData );
+
+		Log.e( "Postgre", "Start" );
+
+		//Desde la version 3 de android, no se permite abrir una conexión de red desde el thread principal.
+		//Por lo tanto se debe crear uno nuevo.
+		sqlThread.start();
+	}
+
+	/*Thread sqlThread = new Thread() {
+		public void run(){
+			try {
+				Class.forName("org.postgresql.Driver");
+				// "jdbc:postgresql://IP:PUERTO/DB", "USER", "PASSWORD");
+				Connection conn = DriverManager.getConnection(
+						"jdbc:postgresql://192.168.0.4:5432/fifa", "test", "test");
+				//En el stsql se puede agregar cualquier consulta SQL deseada.
+				String stsql = "Select version()";
+				Statement st = conn.createStatement();
+				ResultSet rs = st.executeQuery(stsql);
+				rs.next();
+				//---------------------------------------------------
+				Log.e( "Postgre", "Con resultado, fuera de runOnUiThread" );
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						//txtData.setText("#" + rs.getString(1));
+						Log.e( "Postgre", "Con resultado" );
+					}
+				});
+				//---------------------------------------------------
+				//System.out.println( rs.getString(1) );
+				conn.close();
+			} catch (SQLException se) {
+				//System.out.println("oops! No se puede conectar. Error: " + se.toString());
+				Log.e( "Postgre", "No se puede conectar" );
+			} catch (ClassNotFoundException e) {
+				//System.out.println("oops! No se encuentra la clase. Error: " + e.getMessage());
+				Log.e( "Postgre", "No se encuentra la clase" );
+			}
+		}
+	};*/
+
+	Thread sqlThread = new Thread( new Runnable(){
+		public void run(){
+			try {
+				Log.e( "Postgre", "In the Thread -> Start" );
+				Class.forName("org.postgresql.Driver");
+				// "jdbc:postgresql://IP:PUERTO/DB", "USER", "PASSWORD");
+				Connection conn = DriverManager.getConnection("jdbc:postgresql://192.168.0.4:5432/fifa", "test", "test");
+				//En el stsql se puede agregar cualquier consulta SQL deseada.
+				String stsql = "Select version()";
+				Statement st = conn.createStatement();
+				ResultSet rs = st.executeQuery(stsql);
+				rs.next();
+				//---------------------------------------------------
+				Log.e( "Postgre", "Con resultado, fuera de runOnUiThread" );
+				/*runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						//txtData.setText("#" + rs.getString(1));
+						Log.e( "Postgre", "Con resultado" );
+					}
+				});*/
+				//---------------------------------------------------
+				//System.out.println( rs.getString(1) );
+				conn.close();
+			} catch (SQLException se) {
+				//System.out.println("oops! No se puede conectar. Error: " + se.toString());
+				Log.e( "Postgre", "No se puede conectar" );
+			} catch (ClassNotFoundException e) {
+				//System.out.println("oops! No se encuentra la clase. Error: " + e.getMessage());
+				Log.e( "Postgre", "No se encuentra la clase" );
+			} catch( Exception e ){
+				Log.e( "Postgre", "Exception -> " + e.toString() );
+			}
+		}
+	});
+}
